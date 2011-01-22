6511c7c72cad6eded2249f6326e6944cedf90cfc
Hosokawa Hiroyuki <hosokawa@h2factory.org>
2011-01-22 15:19:17 +0900

    Initial init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'edfc103cfc81d99871b3dc26b23d11583d3fc07bce7869d5ae965a2304c73779ee570467aa6a245e6c602dccb2df2583c90d98bbcbcf57a2f0bb9c4137dc09f2'
